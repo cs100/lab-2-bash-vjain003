62db8fc1d1f4eff826ed3a3258a24a077b8c0b7c
Vidhi Jain <vjain003@hammer.cs.ucr.edu>
2018-01-22 15:22:35 -0800

    largest.sh

---
+++ largest.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [[  $# > 3  ||  $# < 3 ]]
+then
+	echo "Please enter 3 numbers only"
+fi
+
+if [[ $1 -gt $2 && $1 -gt $3 ]]
+then
+	echo $1
+fi
+
+if [[ $2 -gt $1 &&  $2 -gt $3 ]]
+then
+	echo $2
+fi
+
+if [[ $3 -gt $2  &&  $3 -gt $1 ]]
+then
+	echo $3
+fi
