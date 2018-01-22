8e26330de165734ff7106678d66436bdeaa6b1e7
Vidhi Jain <vjain003@hammer.cs.ucr.edu>
2018-01-22 15:35:45 -0800

    .hh file generated from createEmptyClassFiles.sh

---
+++ MyClass.hh
@@ -0,0 +1,24 @@
+-e#ifndef MyClass_hh
+#define MyClass_hh
+
+class MyClass
+{
+  public:
+    MyClass();
+    ~MyClass();
+
+  private:
+};
+#endif
+#ifndef MyClass_hh
+#define MyClass_hh
+
+class MyClass
+{
+  public:
+    MyClass();
+    ~MyClass();
+
+  private:
+};
+#endif
